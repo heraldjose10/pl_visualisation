0d190aabac2ae68cda2f9b847fec873747c4a8e1
heraldjose10 <heraldjose10@gmail.com>
2023-05-28 02:22:19 +0100

    add bottom line to navbar links

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,17 +1,24 @@
+'use client'
+
+
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 import { lexend_deca } from "@/utils/fonts";
 
 
 export default function Nav() {
+
+    const pathname:string = usePathname()
+
     return (
-        <ul className={`flex gap-12 ${lexend_deca.variable} font-lexend my-20 [&>li]:cursor-pointer`}>
-            <li>
+        <ul className={`flex gap-12 ${lexend_deca.variable} font-lexend my-20 [&>li]:py-1`}>
+            <li className={pathname == '/' ? 'border-b-2 border-white' : ''}>
                 <Link href='/'>Home</Link>
             </li>
-            <li>
+            <li className={pathname == '/compare' ? 'border-b-2 border-white' : ''}>
                 <Link href='/compare'>Team Compare</Link>
             </li>
-            <li>
+            <li className={pathname == '/about' ? 'border-b-2 border-white' : ''}>
                 <Link href='/about'>About</Link>
             </li>
         </ul>
